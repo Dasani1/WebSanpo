183b2d054cc52d5ae1cb7b81486a353f9e7c350d
Daniel Shan <Danielshan824@gmail.com>
2025-06-21 15:49:19 -0400

    Changed index.html

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\luhui\Code\The-Walking-App\venv
+set VIRTUAL_ENV=C:\Users\danie\OneDrive\Documents\GitHub\WebSanpo\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
